73b46d8abc42c3b3b07861a63a8c852ccaf23348
Sarthak950 <panditsamsar950@gmail.com>
2023-11-26 23:50:27 +0530

    Nearly Completed

--- main.cpp
+++ main.cpp
@@ -1,24 +1,28 @@
-#include <cstdio>
-#include <iostream>
-#include <raylib.h>
+/*
+ * Author:  Sarthak Sharma [Sarthak950]
+ *
+    Making a sand simulator so there are 4 states of a cell
+        1. Empty 
+        2. Rock which are immovable 
+        3. Sand which falls down 
+        4. Water which flows down and to the sides
+*/
+#include "raylib.h"
 #include <bits/stdc++.h>
+#include <string>
 
 // Constsants
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 800;
 const int GRID_SIZE = 200;
 const int CELL_SIZE = 4;
-const int FPS = 7;
+const int FPS = 60;
+
 // not const 
 int grid[GRID_SIZE][GRID_SIZE];
-
-/*
-    we are making a sand simulator so there are 4 states of a cell
-        1. Empty 
-        2. Rock which are immovable 
-        3. Sand which falls down 
-        4. Water which flows down and to the sides
-*/
+int rockCount = 0;
+int sandCount = 0;
+int waterCount = 0;
 
 #define EMPTY 0
 #define ROCK 1
@@ -29,6 +33,11 @@ int grid[GRID_SIZE][GRID_SIZE];
 // set a stage of 3 blocks of rock a little above the ground 
 void InitializeGrid() {
 
+    // Set all cells to Empty
+    for (int i = 0; i < GRID_SIZE; i++)
+        for (int j = 0; j < GRID_SIZE; j++)
+            grid[i][j] = EMPTY;
+
     // Border aroound the grid 
     for (int i = 0; i < GRID_SIZE; i++) {
         grid[GRID_SIZE - 1][i] = ROCK;
@@ -37,18 +46,74 @@ void InitializeGrid() {
         grid[i][0] = ROCK;
     }
 
-    // set a sand in the center 
-    // grid[GRID_SIZE / 2][GRID_SIZE / 2] = SAND;
+ 
 
-    // set some sand in the air 
-    for (int i = 0; i < 40; i++) {
-        grid[GRID_SIZE - 100][(i + 5)*3] = SAND;
+}
+
+void KeyPressHandeler () {
+    int mouseX = GetMouseX();
+    int mouseY = GetMouseY();
+    
+    // mouse position needed to be divided by the cell size to get the grid position
+    mouseX = mouseX / CELL_SIZE;
+    mouseY = mouseY / CELL_SIZE;
+
+
+
+    if (IsKeyDown(KEY_SPACE) ) {
+        grid[mouseX][mouseY] = ROCK;
+    }
+    else if (IsKeyDown(KEY_S) ) {
+        if (grid[mouseX][mouseY] == EMPTY)   grid[mouseX][mouseY] = SAND;
+    }
+    else if (IsKeyDown(KEY_W) ) {
+        if (grid[mouseX][mouseY] == EMPTY)   grid[mouseX][mouseY] = WATER;
+    }
+    else if (IsKeyDown(KEY_E) ) {
+        if (grid[mouseX][mouseY] == EMPTY)   grid[mouseX][mouseY] = EMPTY;
+    }
+    else if (IsKeyPressed(KEY_BACKSPACE)) {
+        InitializeGrid();
     }
 
 }
 
+
+std::string FormatText (const char *text, ...) {
+    char buffer[1024] = { 0 };
+    va_list args;
+    va_start(args, text);
+    vsprintf(buffer, text, args);
+    va_end(args);
+    return buffer;
+}
+
+void DisplayCount () {
+
+    // count the number of rocks, sand and water 
+    rockCount = 0;
+    sandCount = 0;
+    waterCount = 0;
+
+    for (int i = 0; i < GRID_SIZE; i++)
+        for (int j = 0; j < GRID_SIZE; j++) {
+            if (grid[i][j] == ROCK) rockCount++;
+            else if (grid[i][j] == SAND) sandCount++;
+            else if (grid[i][j] == WATER) waterCount++;
+        }
+
+    // Display the count
+    DrawText(FormatText("Rocks: %i", rockCount), 10, 10, 20, BLACK);
+    DrawText(FormatText("Sand: %i", sandCount), 10, 30, 20, BLACK);
+    DrawText(FormatText("Water: %i", waterCount), 10, 50, 20, BLACK);
+
+}
+
 void UpdateGrid() {
-    
+
+    KeyPressHandeler();
+    DisplayCount();
+
     // from bottom left to the top right
     for (int i= 0;i<GRID_SIZE;i++) {
         for ( int j = GRID_SIZE;j>0;j--) {
@@ -57,18 +122,47 @@ void UpdateGrid() {
                     grid[i][j+1] = SAND;
                     grid[i][j] = EMPTY;
                 }
-                else if (grid[i+1][j+1] == EMPTY) {
-                    grid[i+1][j+1] = SAND;
-                    grid[i][j] = EMPTY;
-                }
                 else if (grid[i-1][j+1] == EMPTY) {
                     grid[i-1][j+1] = SAND;
                     grid[i][j] = EMPTY;
                 }
+                else if (grid[i+1][j+1] == EMPTY) {
+                    grid[i+1][j+1] = SAND;
+                    grid[i][j] = EMPTY;
+                }
+                    // add interaction with water 
+                else if (grid[i][j+1] == WATER) {
+                    grid[i][j+1] = SAND;
+                    grid[i][j] = WATER;
+                }
+                else if (grid[i-1][j+1] == WATER) {
+                    grid[i-1][j+1] = SAND;
+                    grid[i][j] = WATER;
+                }
+                else if (grid[i+1][j+1] == WATER) {
+                    grid[i+1][j+1] = SAND;
+                    grid[i][j] = WATER;
+                }
+            }
+            else if (grid[i][j] == WATER) {
+                if (grid[i][j+1] == EMPTY) {
+                    grid[i][j+1] = WATER;
+                    grid[i][j] = EMPTY;
+                }
+                else if (grid[i-1][j] == EMPTY) {
+                    grid[i-1][j] = WATER;
+                    grid[i][j] = EMPTY;
+                }
+                else if (grid[i+1][j] == EMPTY) {
+                    grid[i+1][j] = WATER;
+                    grid[i][j] = EMPTY;
+                }
+                // check for downward diagonal 
+               
+
             }
         }
     }
-
 
 }
 
@@ -110,9 +204,7 @@ int main() {
         // Render the grid
         for (int i = 0; i < GRID_SIZE; i++)
             for (int j = 0; j < GRID_SIZE; j++)
-                // if (grid[i][j])
-                    DrawBox (i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE, grid[i][j]);
-
+                DrawBox (i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE, grid[i][j]);
 
         // Update the grid
         UpdateGrid();
